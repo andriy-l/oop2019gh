7c5fbe4a5c0abf4cfa0f6a544f725e3ba2d55817
Andriy Lutskiv <l.andriy@gmail.com>
2019-06-17 17:38:45 +0300

    add multiple lines in Oleksii

--- src/ua/gov/rada/hello/Main.java
+++ src/ua/gov/rada/hello/Main.java
@@ -3,7 +3,13 @@ package ua.gov.rada.hello;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Oleksii");
+        System.out.println("Oleksii");
+        System.out.println("Oleksii");
+        System.out.println("Oleksii");
+        System.out.println("Oleksii");
+        System.out.println("Oleksii");
+        System.out.println("Oleksii");
         System.out.println("Oleksii");
     }
 }
